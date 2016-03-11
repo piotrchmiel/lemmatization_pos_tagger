971d60e8b3bc180c09ce9a3b03f7ee2aa8cbba76
pchmiel <piotr.chmiel@hotmail.com>
2016-03-11 13:14:35 +0100

    Skrypt tworzacy baze koncowek ze slow w formacie pickle. - zbieramy koncowki 1, 2, 3, 4 literowe - update requirements.txt

---
+++ src/suffix_creator.py
@@ -0,0 +1,54 @@
+from collections import Counter
+from os import path, walk, mkdir
+from pickle import dump
+from bs4 import BeautifulSoup
+from itertools import islice
+
+
+def get_words(filename):
+    with open(filename) as file_handler:
+        tokens = BeautifulSoup(file_handler.read(), 'xml').find_all('tok')
+        for token in tokens:
+            word = token.orth.string
+            if word is not None and len(word) != 1:
+                yield word
+                base = token.base.string
+                if base is not None and len(base) != 1 and base != word:
+                    yield base
+
+
+def find_xml_files(folder_name):
+    for root, dirs, files in walk(folder_name):
+        for filename in files:
+            if filename.endswith(".xml") and not filename.endswith(".rel.xml"):
+                yield path.join(root, filename)
+
+
+def main():
+    CORPUS_DIR = "../CorpusPWr"
+    one_letter_suffixes = Counter()
+    two_letter_suffixes = Counter()
+    three_letter_suffixes = Counter()
+    four_letter_suffixes = Counter()
+
+    if path.exists(CORPUS_DIR):
+        for filename in islice(find_xml_files(CORPUS_DIR), 0, 10):
+            for word in get_words(filename):
+                one_letter_suffixes[word[-1:]] += 1
+                if len(word) > 2:
+                    two_letter_suffixes[word[-2:]] += 1
+                if len(word) > 3:
+                    three_letter_suffixes[word[-3:]] += 1
+                if len(word) > 4:
+                    four_letter_suffixes[word[-4:]] += 1
+
+        if not path.exists("../Suffixes"):
+            mkdir("../Suffixes")
+
+        with open("../Suffixes/suffixes.pickle", 'wb') as file_handler:
+            dump([one_letter_suffixes, two_letter_suffixes, three_letter_suffixes, four_letter_suffixes], file_handler)
+    else:
+        print("There is no Corpus Path!")
+
+if __name__ == '__main__':
+    main()
