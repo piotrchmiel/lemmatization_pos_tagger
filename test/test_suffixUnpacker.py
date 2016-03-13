857b57c82564c7a96b0118333cca142039ed4304
Łukasz Matysiak <december0123@gmail.com>
2016-03-13 18:35:06 +0100

    Add test_suffixUnpacker

---
+++ test/test_suffixUnpacker.py
@@ -0,0 +1,33 @@
+from src.settings import SUFFIX_FILE
+from src.utils import SuffixUnpacker
+from unittest import TestCase
+
+
+class TestSuffixUnpacker(TestCase):
+    def setUp(self):
+        self.unpacker = SuffixUnpacker(SUFFIX_FILE)
+        self.num_of_extracted_suffixes = 10
+
+    def test_extract_n_best_one_letter(self):
+        extracted_one_letter_suffixes = self.unpacker.extract_n_best_one_letter(self.num_of_extracted_suffixes)
+        for suffix_with_count in extracted_one_letter_suffixes:
+            suffix = suffix_with_count[0]
+            self.assertEqual(len(suffix), 1)
+
+    def test_extract_n_best_two_letters(self):
+        extracted_two_letter_suffixes = self.unpacker.extract_n_best_two_letters(self.num_of_extracted_suffixes)
+        for suffix_with_count in extracted_two_letter_suffixes:
+            suffix = suffix_with_count[0]
+            self.assertEqual(len(suffix), 2)
+
+    def test_extract_n_best_three_letters(self):
+        extracted_three_letter_suffixes = self.unpacker.extract_n_best_three_letters(self.num_of_extracted_suffixes)
+        for suffix_with_count in extracted_three_letter_suffixes:
+            suffix = suffix_with_count[0]
+            self.assertEqual(len(suffix), 3)
+
+    def test_extract_n_best_four_letter(self):
+        extracted_four_letter_suffixes = self.unpacker.extract_n_best_four_letter(self.num_of_extracted_suffixes)
+        for suffix_with_count in extracted_four_letter_suffixes:
+            suffix = suffix_with_count[0]
+            self.assertEqual(len(suffix), 4)
