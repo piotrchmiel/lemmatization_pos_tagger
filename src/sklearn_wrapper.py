c986110758da1e4cf17a12bf7b37b1ab461c8e45
Łukasz Matysiak <december0123@gmail.com>
2016-03-20 13:10:00 +0100

    Fix typo

--- src/sklearn_wrapper.py
+++ src/sklearn_wrapper.py
@@ -8,17 +8,17 @@ class SklearnWrapper(object):
 
         self._classifier = classifier
         self._encoder = LabelEncoder()
-        self._vectoricer = DictVectorizer(dtype=dtype, sparse=sparse, sort=sort)
+        self._vectorizer = DictVectorizer(dtype=dtype, sparse=sparse, sort=sort)
 
     def __repr__(self):
         return "<SklearnWrapper(%r)>" % self._classifier
 
     def train(self, X, y):
 
-        X = self._vectoricer.fit_transform(X)
+        X = self._vectorizer.fit_transform(X)
         y = self._encoder.fit_transform(y)
         self._classifier.fit(X, y)
 
     def classify(self, feature_set):
-        feature_set = self._vectoricer.transform(feature_set)
+        feature_set = self._vectorizer.transform(feature_set)
         return self._encoder.classes_[self._classifier.predict(feature_set)][0]
