7406ee95996ef86203bf53879d43dc3ba429be5e
kmachnicki <kamil0602@gmail.com>
2016-03-22 00:11:54 +0100

    Moved csv files under common dir

--- src/settings.py
+++ src/settings.py
@@ -4,10 +4,11 @@ BASE_DIR = path.dirname(path.dirname(__file__))
 SUFFIX_FILE = path.join(BASE_DIR, 'Suffixes', 'suffixes.pickle')
 CLASSIFIERS_DIR = path.join(BASE_DIR, 'Classifiers')
 SUFFIX_DIR = path.join(BASE_DIR, 'Suffixes')
+OUTPUT_CSV = path.join(BASE_DIR, 'OutputCsv')
 CORPUS_DIR = path.join(BASE_DIR, 'CorpusPWr')
-CORPUS_CSV = path.join(CORPUS_DIR, 'extracted.csv')
-NATIONAL_CORPUS_DIR = path.join(BASE_DIR, 'NationalCorpus')
-NATIONAL_CORPUS_CSV = path.join(NATIONAL_CORPUS_DIR, 'extracted.csv')
+CORPUS_CSV = path.join(OUTPUT_CSV, 'CorpusPWr')
+NATIONAL_CORPUS_DIR = path.join(BASE_DIR, 'NationalCorpus.csv')
+NATIONAL_CORPUS_CSV = path.join(OUTPUT_CSV, 'NationalCorpus.csv')
 
 SUFFIX_TAGGER = "suffix_tagger.pickle"
 N_ONE_LETTER = 15
