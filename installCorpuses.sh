2318a8623adf4ed837d9936d2023e695a42c418a
kmachnicki <kamil0602@gmail.com>
2016-03-22 00:04:08 +0100

    Removed 3GB of xml files, added script for installing corpuses

---
+++ installCorpuses.sh
@@ -0,0 +1,39 @@
+echo "***** Checking if needed packages are installed *****"
+
+if ! which 7z > /dev/null; then
+    echo "No 7z found. Please install it first."
+    exit
+fi
+
+if ! which wget > /dev/null; then
+    echo "No wget found. Please install it first."
+    exit
+fi
+
+if ! which tar > /dev/null; then
+    echo "No tar found. Please install it first."
+    exit
+fi
+
+
+echo "***** Unpacking CorpusPwr *****"
+
+if [ ! -d "CorpusPWr" ]; then
+    mkdir "CorpusPWr"
+fi
+
+#7z x ./CorpusPWr/kpwr-1.2.6-disamb.7z -o./CorpusPWr/ -y -r
+
+echo "***** Downloading NationalCorpus 1.2 *****"
+
+if [ ! -d "NationalCorpus" ]; then
+    mkdir "NationalCorpus"
+fi
+
+wget -O ./NationalCorpus/NKJP-PodkorpusMilionowy-1.2.tar.gz "http://clip.ipipan.waw.pl/NationalCorpusOfPolish?action=AttachFile&do=get&target=NKJP-PodkorpusMilionowy-1.2.tar.gz"
+
+echo "***** Unpacking NationalCorpus *****"
+
+tar -zxvf ./NationalCorpus/NKJP-PodkorpusMilionowy-1.2.tar.gz
+
+echo "***** All done *****"
