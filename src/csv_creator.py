9b1a8439cfc990d042e7fb8ce8a0e997f6b60ece
Łukasz Matysiak <december0123@gmail.com>
2016-03-21 20:56:12 +0100

    Remove hardcoded string

--- src/csv_creator.py
+++ src/csv_creator.py
@@ -12,7 +12,7 @@ def detect_national_corpus(path):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--corpus", dest="corpus", help="path to corpus directory", default="../CorpusPWr")
+    parser.add_argument("-c", "--corpus", dest="corpus", help="path to corpus directory", default=CORPUS_DIR)
     args = parser.parse_args()
     if os.path.exists(args.corpus):
         print("Start CSV extracting...")
