e132d2db277eeefbd32357250a9bccedc31e5ab8
pchmiel <piotr.chmiel@hotmail.com>
2016-03-21 22:21:24 +0100

    Change rel path into absolute path ;-)

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,14 @@
-SUFFIX_FILE = "../Suffixes/suffixes.pickle"
-CLASSIFIERS_DIR = "../Classifiers"
-SUFFIX_DIR = "../Suffixes"
-CORPUS_DIR = "../CorpusPWr"
+from os import path
+
+BASE_DIR = path.dirname(path.dirname(__file__))
+SUFFIX_FILE = path.join(BASE_DIR, 'Suffixes', 'suffixes.pickle')
+CLASSIFIERS_DIR = path.join(BASE_DIR, 'Classifiers')
+SUFFIX_DIR = path.join(BASE_DIR, 'Suffixes')
+CORPUS_DIR = path.join(BASE_DIR, 'CorpusPWr')
+CORPUS_CSV = path.join(CORPUS_DIR, 'extracted.csv')
+NATIONAL_CORPUS_DIR = path.join(BASE_DIR, 'NationalCorpus')
+NATIONAL_CORPUS_CSV = path.join(NATIONAL_CORPUS_DIR, 'extracted.csv')
+
 SUFFIX_TAGGER = "suffix_tagger.pickle"
 N_ONE_LETTER = 15
 N_TWO_LETTERS = 35
