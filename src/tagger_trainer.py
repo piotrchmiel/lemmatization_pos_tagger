68e07c125f4745cf9d8b0ba9662dfa977e829f37
pchmiel <piotr.chmiel@hotmail.com>
2016-03-22 21:31:36 +0100

    Refaktor Trainer

--- src/tagger_trainer.py
+++ src/tagger_trainer.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import BernoulliNB
@@ -12,22 +14,14 @@ from src.factories.tagger_factory import TaggerFactory
 def main():
     factory = TaggerFactory()
 
-    factory.create_tagger(DecisionTreeClassifier(), False, "tagger_decision_tree")
-    factory.create_tagger(SGDClassifier(), False, "sdg")
-    factory.create_tagger(LogisticRegression(), False, "logistic_regression")
-    factory.create_tagger(BernoulliNB(), False, "naive_bayes")
-    factory.create_tagger(SVC(), False, "svm")
-    factory.create_tagger(KNeighborsClassifier(), False, "kneighbors")
-    factory.create_tagger(Classifier(layers=[Layer("Rectifier", units=100), Layer("Softmax")],
-                                     learning_rate=0.02, n_iter=10), False, "neural_network")
-    factory.create_tagger(DecisionTreeClassifier(), True, "tagger_decision_tree")
-    factory.create_tagger(SGDClassifier(), True, "sdg")
-    factory.create_tagger(LogisticRegression(), True, "logistic_regression")
-    factory.create_tagger(BernoulliNB(), True, "naive_bayes")
-    factory.create_tagger(SVC(), True, "svm")
-    factory.create_tagger(KNeighborsClassifier(), True, "kneighbors")
-    factory.create_tagger(Classifier(layers=[Layer("Rectifier", units=100), Layer("Softmax")],
-                                     learning_rate=0.02, n_iter=10), True, "neural_network")
+    algorithms = {'tagger_decision_tree': DecisionTreeClassifier(), 'sdg': SGDClassifier(), 'svm': SVC(),
+                  'logistic_regression': LogisticRegression(), 'naive_bayes': BernoulliNB(),
+                  'kneighbors': KNeighborsClassifier(), 'neural_network' : Classifier(layers=[Layer("Rectifier",
+                   units=100), Layer("Softmax")], learning_rate=0.02, n_iter=10)}
+
+    for filename, algorithm in algorithms.items():
+        factory.create_tagger(deepcopy(algorithm), False, filename)
+        factory.create_tagger(deepcopy(algorithm), False, filename)
 
 if __name__ == '__main__':
     main()
