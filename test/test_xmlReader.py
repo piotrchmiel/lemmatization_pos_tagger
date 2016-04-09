71d65ad79d8692f696c7d4fdc9ee861b54af3b21
Łukasz Matysiak <december0123@gmail.com>
2016-04-09 15:16:55 +0200

    Add empty test_xmlReader.py

---
+++ test/test_xmlReader.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from src.utils.xml_reader import XmlReader
+
+
+class TestXmlReader(TestCase):
+    def setUp(self):
+        pass
+
+    def test_extract_words_and_tags(self):
+        self.fail()
+
+    def test_get_words(self):
+        self.fail()
+
+    def test_get_words_pwr(self):
+        self.fail()
+
+    def test_get_words_national(self):
+        self.fail()
+
+    def test_find_xml_files(self):
+        self.fail()
