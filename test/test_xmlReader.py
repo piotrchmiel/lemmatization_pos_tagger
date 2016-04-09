6e1ac7e458abdcbb1885c639ef76001dd35e7fd2
Łukasz Matysiak <december0123@gmail.com>
2016-04-09 16:54:15 +0200

    Test xmlReader

--- test/test_xmlReader.py
+++ test/test_xmlReader.py
@@ -5,19 +5,16 @@ from src.utils.xml_reader import XmlReader
 
 class TestXmlReader(TestCase):
     def setUp(self):
-        pass
-
-    def test_extract_words_and_tags(self):
-        self.fail()
-
-    def test_get_words(self):
-        self.fail()
-
-    def test_get_words_pwr(self):
-        self.fail()
-
-    def test_get_words_national(self):
-        self.fail()
+        self.test_corpus_dir = "TestCorpus"
+        self.xmlReader = XmlReader(self.test_corpus_dir, use_national_corpus=True)
 
     def test_find_xml_files(self):
-        self.fail()
+        expected_number_of_xml_files = 1
+        self.assertEqual(expected_number_of_xml_files, len(list(self.xmlReader.find_xml_files())))
+
+    def test_extract_words_and_tags(self):
+        expected_number_of_extracted_pairs = 3
+        expected_pairs = [('Powód', 'rzeczownik'), ('był', 'pseudoimiesłów'), ('prosty', 'przymiotnik')]
+        extracted_pairs = list(self.xmlReader.extract_words_and_tags())
+        self.assertEqual(expected_number_of_extracted_pairs, len(extracted_pairs))
+        self.assertSequenceEqual(expected_pairs, extracted_pairs)
