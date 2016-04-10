70fd7fccf92e0563661f41b69ed26b20c1427335
Michal Polanski <mpolanski@users.noreply.github.com>
2016-04-10 23:16:35 +0200

    time measurement of train function

--- src/tagger_trainer.py
+++ src/tagger_trainer.py
@@ -1,3 +1,4 @@
+import sys, getopt
 from copy import deepcopy
 from itertools import chain
 from joblib import Parallel, delayed
@@ -12,7 +13,25 @@ from sknn.mlp import Classifier, Layer
 from src.factories.tagger_factory import TaggerFactory
 
 
-def main():
+def main(argv):
+
+    parallel_option = ''
+    try:
+        opts, args = getopt.getopt(argv,'hp:', ['help', 'parallel='])
+    except getopt.GetoptError:
+        print('tagger_trainer.py --parallel <y/n>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print('tagger_trainer.py --parallel <y/n>')
+            sys.exit()
+        elif opt in ("-p", "--parallel"):
+            if arg not in ('y', 'n'):
+                print("--parallel argument takes 'y' or 'n' input. Given: ", arg)
+                sys.exit(2)
+            parallel_option = arg
+
+
     factory = TaggerFactory()
 
     algorithms = {'tagger_decision_tree': DecisionTreeClassifier(), 'sdg': SGDClassifier(), 'svm': SVC(),
@@ -23,10 +42,18 @@ def main():
                                                        Layer("Softmax")],
                                                learning_rate=0.02, n_iter=10)}
 
-    Parallel(n_jobs=-1)(chain((delayed(factory.dump_tagger)(deepcopy(algorithm), filename, True)
-                        for filename, algorithm in algorithms.items()),
-                        (delayed(factory.dump_tagger)(deepcopy(algorithm), filename, False)
-                        for filename, algorithm in algorithms.items())))
+    if (parallel_option == 'n'):
+        print("Parallelization is disabled")
+        for filename, algorithm in algorithms.items():
+            factory.dump_tagger(deepcopy(algorithm), filename, True)
+            factory.dump_tagger(deepcopy(algorithm), filename, False)
+
+    else:
+        print("Parallelization is enabled")
+        Parallel(n_jobs=-1)(chain((delayed(factory.dump_tagger)(deepcopy(algorithm), filename, True)
+                            for filename, algorithm in algorithms.items()),
+                            (delayed(factory.dump_tagger)(deepcopy(algorithm), filename, False)
+                            for filename, algorithm in algorithms.items())))
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
